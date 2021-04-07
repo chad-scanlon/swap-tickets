21066c5b3a53ee7d576c5539ec13f7a8d882e2b4
chad-scanlon <59572157+chad-scanlon@users.noreply.github.com>
2021-04-07 16:21:05 -0400

    testing- adding console logs

--- src/Components/Tickets.js
+++ src/Components/Tickets.js
@@ -7,7 +7,8 @@ import { Link } from "react-router-dom";
 
 const Tickets = (props) => {
     const { tickets, setTickets } = useContext(InitialContext);
-
+    console.log(props);
+    console.log(tickets);
     return (
         <>
             <h1 className="ticket-header">Locates</h1>
